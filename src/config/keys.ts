3ba4d02d0c7feec0b556d78a7be88e81a77349fe
jonomk07 <jono.muamba@outlook.com>
2022-03-23 16:59:26 +0200

    removed key

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,3 +1,3 @@
 export default {
-    mongoURI: 'mongodb+srv://jonomk:<password here>@cluster0.lksrl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://<Username here >:<password here>@cluster0.lksrl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   };
